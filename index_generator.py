16b0260e3845edc3c917ef5a79681db50160fe9c
Zhaoyuan Ma <zma3@wpi.edu>
2020-06-14 16:19:44 -0400

    add notes

--- index_generator.py
+++ index_generator.py
@@ -2,15 +2,15 @@
 Based on https://www.polarxiong.com/archives/Python-%E7%94%9F%E6%88%90%E7%9B%AE%E5%BD%95%E6%A0%91.html
 This script do two things:
 1. Check all folders, create a HTML menu as list.
-2. Chekc all blog files, replace "*.md" with "*.html"
+2. Chekc all blog files, replace "*.md" with "*.html". (TODO)
 '''
 import os
 import os.path
 
 BRANCH = '├─'
 LAST_BRANCH = '└─'
-TAB = '│  '
-EMPTY_TAB = '   '
+TAB = '│&nbsp;&nbsp;'
+EMPTY_TAB = '&nbsp;&nbsp;&nbsp;'
 
 def process_html(filename):
     f = open(filename, "r", encoding='utf8')
@@ -23,35 +23,38 @@ def get_dir_list(path, placeholder='', pastpass=''):
     result = ''
     ph = placeholder
 
-    for idx, folder in enumerate(folder_list):
-        t = TAB
-        b = BRANCH
-        if not idx == len(folder_list)-1:
-            if not file_list:
-                t = EMPTY_TAB
-                b = LAST_BRANCH
-
-        sublist = get_dir_list(os.path.join(path, folder), placeholder + t, pastpass + folder + '/')
-        # skip empty folder
-        if 'html' in sublist:
-            result += placeholder + b + '<b>' + folder + '</b>' + '<br />' + sublist
-
+    # result += ph + TAB + '<br />'
 
     for idx, file in enumerate(file_list):
-        if idx == len(file_list)-1:
+        if idx == len(file_list)-1 and len(folder_list) == 0:
             b = LAST_BRANCH
         else:
             b = BRANCH
         process_html(pastpass+file)
         result += ph + b + '<a href="' + pastpass + file + '">' + file + '</a><br />'
 
-    result += ph+'<br />'
+    if len(folder_list) == 0:
+        result += ph + '<br />'
+    else:
+        result += ph + TAB + '<br />'
+
+    for idx, folder in enumerate(folder_list):
+        t = TAB + EMPTY_TAB
+        b = BRANCH
+        if idx == len(folder_list)-1:
+            t = EMPTY_TAB + EMPTY_TAB
+            b = LAST_BRANCH
+
+        sublist = get_dir_list(os.path.join(path, folder), ph + t, pastpass + folder + '/')
+        # skip empty folder
+        if 'html' in sublist:
+            result += ph + b + '<b>' + folder + '</b>' + '<br />' + sublist
 
     return result
 
 
 if __name__ == '__main__':
-    htmlfile = get_dir_list(os.getcwd(),'')
+    htmlfile = get_dir_list(os.getcwd())
     f = open('index.html', 'w', encoding='utf8')
     htmlfile = '<!doctype html>\n<html>\n<head></head><body>' + htmlfile + '</body>'
     f.write(htmlfile)
